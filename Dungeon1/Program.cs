2caf394d9f415caad605c1f403f36760c15588cc
Алексей Семенов <Leon4ik_1@mail.ru>
2020-09-27 22:13:29 +0500

    vse

--- Dungeon1/Program.cs
+++ Dungeon1/Program.cs
@@ -6,11 +6,59 @@ using System.Threading.Tasks;
 
 namespace Dungeon1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static Random random = new Random();
+
+        static int LinearSearch(int[] array, int element)
         {
-            Console.WriteLine("11");
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] == element) return i;
+            return -1;
+        }
+
+        static int SearchUnique(int[] array, int element)
+        {
+            return array.Distinct().Count();
+        }
+
+        static int[] GenerateSortedArray(int length)
+        {
+            var array = new int[length];
+            for (int i = 1; i < array.Length; i++)
+                array[i] = array[i - 1] + random.Next(10000) + 1;
+            return array;
+        }
+
+        static int SortedArray(int[] array, int element)
+        {
+            Array.Sort(array);
+            return 0;
+        }
+
+        static long MeasureTime(int[] array, Func<int[], int, int> searchProcedure)
+        {
+            searchProcedure(array, array[random.Next(array.Length)]);
+            var repetitions = 10000;
+            var watch = new Stopwatch();
+            watch.Start();
+            for (int i = 0; i < repetitions; i++)
+                searchProcedure(array, array[random.Next(array.Length)]);
+            watch.Stop();
+            return watch.ElapsedMilliseconds;
+        }
+
+        static void Main()
+        {
+            int[] array = GenerateSortedArray(100000);
+            long linSearch = MeasureTime(array, LinearSearch);
+            long searchUnique = MeasureTime(array, SearchUnique);
+            long sortedArray = MeasureTime(array, SortedArray);
+            Console.WriteLine("Количество элиментов в массиве - "+ array.Count() +" Линейный поиск - " + linSearch);
+            Console.WriteLine("Количество элиментов в массиве - " + array.Count() + " Поиск уникальных слов - " + searchUnique);
+            Console.WriteLine("Количество элиментов в массиве - " + array.Count() + " Сортировка Массива - " + sortedArray);
+            Console.ReadKey();
         }
     }
 }
+
